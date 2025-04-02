268e7ea27bab4ccf7a2065112e9a61669384412a
Harsh <82308380+WhoHarsh-14@users.noreply.github.com>
2025-04-02 18:58:24 +0530

    forgot to commit the new file.

---
+++ src/main/java/me/harsh/mbedwarskitsaddon/commands/KitMenuCommand.java
@@ -0,0 +1,34 @@
+package me.harsh.mbedwarskitsaddon.commands;
+
+import de.marcely.bedwars.api.player.PlayerDataAPI;
+import me.harsh.mbedwarskitsaddon.config.KitConfig;
+import me.harsh.mbedwarskitsaddon.kits.KitManager;
+import me.harsh.mbedwarskitsaddon.menu.KitMenu;
+import me.harsh.mbedwarskitsaddon.utils.KitsUtil;
+import org.bukkit.entity.Player;
+
+public class KitMenuCommand extends SubCommand {
+  public KitMenuCommand() {
+    super("menu");
+  }
+
+  @Override
+  public void onCommand(Player player, String[] args) {
+//    if (!player.hasPermission(KitsUtil.KIT_ADMIN_PERM))
+//      return;
+
+    // /kit menu
+    if (args.length != 0) {
+      KitsUtil.tell(player, KitConfig.getMessagesMap().get("Command_invalid"));
+      return;
+    }
+
+    // Open the menu
+    PlayerDataAPI.get().getProperties(player.getUniqueId(), playerProperties -> {
+      final KitMenu menu = new KitMenu(playerProperties);
+      menu.draw(player);
+      menu.open(player);
+    });
+  }
+
+}
